615bf6bc21fe50ae2394ef31bae2a196d4cbf843
Alan Zhang <gagazcfan@icloud.com>
2024-04-05 17:32:25 +0800

    allow navigate between multiple scenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import StudyRoom from "./features/study-room";
+import { useState } from "react";
 import Navigator from "./navigator";
+import StudyRoom from "./features/study-room";
+import DisplacedTorus from "./features/displaced-torus";
 
 export default function App() {
   return (
     <>
       <Navigator />
-      <StudyRoom />
+      <Scenes />
+    </>
+  );
+}
+
+const SCENES = [StudyRoom, DisplacedTorus];
+
+function Scenes() {
+  const [ind, setInd] = useState(0);
+
+  const Scene = SCENES[ind];
+  return (
+    <>
+      {ind > 0 && (
+        <div
+          onClick={() => setInd((ind) => ind - 1)}
+          className="navScene prevScene"
+        />
+      )}
+      {ind < SCENES.length - 1 && (
+        <div
+          onClick={() => setInd((ind) => ind + 1)}
+          className="navScene nextScene"
+        />
+      )}
+      <Scene />
     </>
   );
 }
---
+++ src/features/displaced-torus/index.tsx
@@ -0,0 +1,17 @@
+import Loading from "../../components/loading";
+
+export default function DisplacedTorus() {
+  return (
+    <>
+      <span
+        style={{
+          display: "block",
+          marginBottom: 50,
+        }}
+      >
+        <b>TODO:</b> Displaced Torus
+      </span>
+      <Loading loading={true} />
+    </>
+  );
+}
